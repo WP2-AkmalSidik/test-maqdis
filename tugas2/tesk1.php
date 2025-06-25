add7686026663fbe50c44709452cefc65ba8b4af
Akmal Sidik <143138356+WP2-AkmalSidik@users.noreply.github.com>
2025-06-25 11:33:24 +0700

    feat(task1): menyatukan berbagai string menjadi 1 kalimat

---
+++ tugas2/tesk1.php
@@ -0,0 +1,9 @@
+<?php
+$variable1 = "MAQDIS";
+$variable2 = 'Academy';
+$variable3 = 'is';
+$variable4 = 'awesome';
+
+$words = [$variable1, $variable2, $variable3, $variable4];
+echo implode(' ', $words);
+?>
