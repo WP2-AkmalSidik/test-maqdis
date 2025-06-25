67ae12bb3edd5322faf3d5c1df0c4a5a888ba3c4
Akmal Sidik <143138356+WP2-AkmalSidik@users.noreply.github.com>
2025-06-25 11:53:55 +0700

    feat(tugas4): kode untuk menjawab tugas looping

---
+++ tugas4/looping.php
@@ -0,0 +1,11 @@
+<?php
+echo "LOOPING PERTAMA\n";
+for ($i = 2; $i <= 20; $i += 2) {
+    echo "$i - I love coding\n";
+}
+
+echo "\nLOOPING KEDUA\n";
+for ($i = 20; $i >= 2; $i -= 2) {
+    echo "$i - I will become a mobile developer\n";
+}
+?>
